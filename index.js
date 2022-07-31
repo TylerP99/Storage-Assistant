2227f01adfeeddb65482e4ad07ac642f0160e956
Tyler Perata <tylerperata@gmail.com>
2022-07-31 14:07:21 -0700

    Add mongodb connection

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ app.use(express.json()); // Parses JSON requests and puts the data in body
 app.use(express.urlencoded({ extended:true})); // Parses incoming url options and puts the data in the body
 app.use(express.static("public")); // Allows router to easily grab from public folder (mainly used for js and css on client side)
 
+// Mongoose Config
+const mongoose = require("mongoose");
+
+const dbName = "StorageApp";
+const dbConnectionString = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose.connect(
+    dbConnectionString,
+    //Options
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then( _ => {
+    console.log(`Connected to ${dbName} succesfully`);
+})
+.catch(e => {
+    console.error(e);
+}) 
+
 
 // Root Router
 app.use("/", require("./routes/index.js"));
@@ -22,4 +43,4 @@ app.use("/api", require("./routes/api.js"));
 
 app.listen(PORT, () =>{
     console.log(`Listening on ${PORT}`);
-})
+});
--- routes/api/account.js
+++ routes/api/account.js
@@ -5,35 +5,62 @@ const router = express.Router();
 const User = require("../../models/User.js");
 
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 //Create account
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
     const account = req.body.account;
-    console.log(req.body.account);
-    res.json("Received");
 
     const valid = validate_account(account);
 
-    if(!valid) 
-    {
-        // Send back info
+    if(valid) {
+        // Add to database, send back success
+        bcrypt.hash(account.password1, saltRounds, async (err, hash) => {
+            const newAccount = {
+                email: account.email,
+                password: hash
+            }
+            await User.create(newAccount);
+        });
     }
 
-    // Add to database, send back success
-
+    res.json(valid);
 });
 
 async function validate_account(account) {
-    let valid = true;
+    const MIN_PASSWORD_LEN = 6;
+
+    // Validation object that contains overall valid boolean, and error booleans for client side warnings
+    let validation = {
+        valid: true,
+        errors: {
+            emailInUse: false,
+            passwordWeak: false,
+            passwordNotMatch: false
+        }
+    };
     // Ensure email is unique
+    const emailInUse = await User.findOne({email: account.email});
+
+    if(emailInUse) {
+        validation.valid = false;
+        validation.errors.emailInUse = true;
+    }
 
     // Ensure password1 meets requirements
+    if(account.password1.length < MIN_PASSWORD_LEN) {
+        validation.valid = false;
+        validation.errors.passwordWeak = true;
+    }
 
     // Ensure password2 matches password1
+    if(account.password1 !== account.password2) {
+        validation.valid = false;
+        validation.errors.passwordNotMatch = true;
+    }
 
-    // For now, just return true so I can make database additions
-    return valid;
+    return validation;
 }
 
 module.exports = router
