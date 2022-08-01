7a30a02d17181b220b77807bdcf33a7d68f51fb9
Tyler Perata <tylerperata@gmail.com>
2022-08-01 16:03:52 -0700

    Auth middleware add

---
+++ config/auth-middleware.js
@@ -0,0 +1,15 @@
+// Middleware to forward user if they are already authenticated and to ensure user is authenticated to access routes
+module.exports = {
+    ensureAuthenticated: (req,res,next) => {
+        if(req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/login");
+    },
+    forwardIfAuthenticated: (req,res,next) => {
+        if(!req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/storage");
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
+const { ensureAuthenticated, forwardIfAuthenticated } = require("../config/auth-middleware.js");
+
 
 
 // Root/Landing Route
-router.get("/", /*forwardAuthenticate,*/ (req, res) => {
+router.get("/", (req, res) => {
     console.log("GET landing");
     res.render("landing.ejs");
 });
 
 // Registration route
-router.get("/register", /*forwardAuthenticate,*/ (req, res) => {
+router.get("/register", forwardIfAuthenticated, (req, res) => {
     console.log("GET register");
     res.render("register.ejs");
 });
 
 // Login route
-router.get("/login", /*forwardAuthenticate,*/ (req,res) => {
+router.get("/login", forwardIfAuthenticated, (req,res) => {
     console.log("GET login");
     res.render("login.ejs");
 });
 
 // Storage Route
-router.get("/storage", (req, res) => {
+router.get("/storage", ensureAuthenticated, (req, res) => {
     res.render("storage.ejs");
 });
 
