3129383e15bbebc8a22448325d6c26278413e29c
Tyler Perata <tylerperata@gmail.com>
2022-08-01 23:50:57 -0700

    Storage Location update route add

--- routes/api/storage.js
+++ routes/api/storage.js
@@ -56,8 +56,60 @@ function is_valid_storage_location(location) {
 }
 
 // Edit StorageLocation properties
+router.put("/update/StorageLocation", (req, res, next) => {
+    // Construct object from req
+    const updatedLocation = {
+        name: req.body.location.name,
+        description: req.body.location.description,
+        length: req.body.location.length,
+        width: req.body.location.width,
+        height: req.body.location.height
+    };
+
+    // Verify object again baby
+    const validation = is_valid_storage_location(updatedLocation);
+
+    if(!validation.valid) {
+        return res.status(400).json(validation);
+    }
+
+    // Update object by id
+    try {
+        await StorageLocation.findByIdAndUpdate(
+            req.body.location.id, // Id to find
+            updatedLocation, // Properties to update
+            { // Options
+                upsert: false // Dont make a new one if the one to update isnt found..
+            }
+        )
+
+        return res.status(200).json(validation);
+    }
+    catch(e) {
+        console.error(e);
+        next(e);
+    }
+});
 
 // Add object to StorageLocation (put) (object is either item or container)
+router.put("/add/StorageLocation", (req,res,next) => {
+    // Determine resource to be created
+    let validation = {
+        newObj: {},
+        valid: false,
+        error: "Requested resource type does not exist"
+    }
+    if(req.body.type == "container") {
+        validation = create_new_container(req.body);
+    }
+    else if (req.body.type == "item") {
+        validation = create_new_item(req.body);
+    }
+    else {
+        // Invalid req
+        return res.status(400).json(validation);
+    }
+});
 
 // Delete StorageLocation (and all contents!!)
 
