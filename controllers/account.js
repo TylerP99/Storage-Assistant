926d6573720609ee6a63333954b3aadd91735849
Tyler Perata <tylerperata@gmail.com>
2022-08-17 15:27:00 -0700

    Convert account handler to follow mvc

---
+++ controllers/account.js
@@ -0,0 +1,117 @@
+//New user controller
+const User = require("../models/User.js");
+
+const passport = require("passport");
+
+const AccountController = {
+
+    // Create account
+    /*
+        body: {
+            new account information to be added
+            account: {
+                email
+                password1
+                password2
+            }
+        }
+    */
+    create_account: async (req, res, next) => {
+        const account = req.body.account;
+        const saltRounds = 10
+
+        const validation = await AccountController.validate_account(account);
+
+        if(!validation.valid) {
+            return res.status(400).json(validation);
+        }
+
+        try{
+            const hashedPassword = await bcrypt.hash(account.password1, saltRounds);
+            const newAccount = {
+                email: account.email,
+                password: hashedPassword
+            };
+
+            await User.create(newAccount);
+
+            res.status(200).json(validation);
+        }
+        catch(e) {
+            console.error(e);
+            next(e);
+        }
+    },
+
+    // Authenticate account
+    /*
+        body: {
+            email,
+            password
+        }
+        Used by passport local strategy
+    */
+    authenticate_account: (req,res,next) => {
+        passport.authenticate(
+            "local",
+            (err, user, info, status) => {
+                if(err) {
+                    console.error(err);
+                    return next(err);
+                }
+
+                if(!user) {
+                    console.error("No user");
+                    return res.status(400).json(
+                        {success:false,
+                        error:info.message}
+                    );
+                }
+
+                req.login(user, (err) => {
+                    if(err) {
+                        return next(err);
+                    }
+                    res.status(200).json({success:true});
+                });
+            }
+        )(req,res,next);
+    },
+
+    // Update account
+
+    // Delete account
+
+    // Validate account
+    validate_account: async (account) => {
+        const MIN_PASSWORD_LEN = 8;
+
+        const validation = {
+            valid: true,
+            errors: []
+        };
+
+        const emailInUse = await User.findOne(
+            { email: account.email }
+        );
+
+        if(emailInUse) {
+            validation.valid = false;
+            validation.errors.push("Email in use");
+        }
+
+        if(account.password1.length < MIN_PASSWORD_LEN) {
+            validation.valid = false;
+            validation.errors.push("Password too short");
+        }
+
+        if(account.password1 !== account.password2) {
+            validation.valid = false;
+            validation.errors.push("Passwords do not match");
+        }
+
+        return validation;
+    }
+}
+
+module.exports = AccountController;
--- routes/api/account.js
+++ routes/api/account.js
@@ -2,103 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const passport = require("passport");
+const AccountController = require("../../controllers/account.js");
 
-const User = require("../../models/User.js");
+router.post("/create", AccountController.create_account);
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-
-//Create account
-router.post("/create", async (req, res) => {
-    const account = req.body.account;
-
-    const valid = await validate_account(account);
-
-    if(valid.valid) {
-        // Add to database, send back success
-        try {
-            const hashedPassword = await bcrypt.hash(account.password1, saltRounds);
-            const newAccount = {
-                email: account.email,
-                password: hashedPassword
-            }
-            await User.create(newAccount);
-            console.log("Account Created");
-        }
-        catch(e) {
-            console.error(e);
-            // Send bad res
-        }
-    }
-
-    res.json(valid);
-});
-
-async function validate_account(account) {
-    const MIN_PASSWORD_LEN = 6;
-
-    console.log(account);
-
-    // Validation object that contains overall valid boolean, and error booleans for client side warnings
-    let validation = {
-        valid: true,
-        errors: {
-            emailInUse: false,
-            passwordWeak: false,
-            passwordNotMatch: false
-        }
-    };
-    // Ensure email is unique
-    const emailInUse = await User.findOne({email: account.email});
-
-    if(emailInUse) {
-        validation.valid = false;
-        validation.errors.emailInUse = true;
-    }
-
-    // Ensure password1 meets requirements
-    if(account.password1.length < MIN_PASSWORD_LEN) {
-        validation.valid = false;
-        validation.errors.passwordWeak = true;
-    }
-
-    // Ensure password2 matches password1
-    if(account.password1 !== account.password2) {
-        validation.valid = false;
-        validation.errors.passwordNotMatch = true;
-    }
-
-    return validation;
-}
-
-
-
-
-//LOGIN/Authenticate Account Route
-router.post("/authenticate", (req,res,next) => {
-    console.log("Passport auth");
-
-    passport.authenticate("local", (e, user, info, status) => {
-        if(e) {
-            console.log("err")
-            return next(err);
-        }
-        if(!user) {
-            console.log("res sent")
-            return res.status(401).json({success:false, error:info.message}).end();
-        }
-
-        req.login(user, err => {
-            console.log("Log in");
-            if(err) {
-                return next(err);
-            }
-            res.json({success:true, error:""});
-        })
-    })(req,res,next);
-});
-
+router.post("/authenticate", AccountController.authenticate_account)
 
 module.exports = router;
